b037130bff205d580fdac7c925b63396dbaeea43
Anton Warkentin <reddoshoruda@gmail.com>
2023-07-12 23:10:18 +0500

    Add project files.

---
+++ Core/BasePage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using Core;
+
+namespace Core
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver driver;
+        protected string url;
+
+        public BasePage()
+        {
+            driver = Browser.Instance.Driver;
+        }
+    }
+}
