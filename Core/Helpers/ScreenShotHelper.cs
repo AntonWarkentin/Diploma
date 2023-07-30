4507a4507011dd4dc9ab37c3fe7b5d05c9580b82
Anton Warkentin <reddoshoruda@gmail.com>
2023-07-30 22:09:24 +0500

    + screenshot fixes

--- Core/Helpers/ScreenShotHelper.cs
+++ Core/Helpers/ScreenShotHelper.cs
@@ -7,9 +7,12 @@ namespace Core.Helpers
     {
         public static void TakeScreenShot(this IWebDriver driver)
         {
-            var screenShotName = TestContext.CurrentContext.Test.FullName + DateTime.Now.ToString("dd-MM-yyyy hh.mm.ss");
+            Directory.CreateDirectory("screenshots");
+
+            var screenShotName = TestContext.CurrentContext.Test.Name + DateTime.Now.ToString("dd-MM-yyyy hh.mm.ss");
             Screenshot screenShot = ((ITakesScreenshot)driver).GetScreenshot();
-            screenShot.SaveAsFile($"./{screenShotName}.png", ScreenshotImageFormat.Png);
+
+            screenShot.SaveAsFile($"./screenshots/{screenShotName}.png", ScreenshotImageFormat.Png);
         }
     }
 }
